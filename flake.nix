f7a71d6c600957653902d707d224b9b444910728
AsPulse / あすぱる <contact@aspulse.dev>
2024-06-05 01:04:27 +0900

    chore: flake.nix, rust-toolchain.toml

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Sutera developing environment of Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            libiconv
+          ];
+          buildInputs = with pkgs; [
+            rustup
+          ];
+          shellHook = ''
+            exec $SHELL
+          '';
+        };
+      }
+    );
+}
