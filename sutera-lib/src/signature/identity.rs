8d0a1f9eaae34cb720acb4388ec4f92822fcf099
AsPulse / あすぱる <contact@aspulse.dev>
2024-06-06 20:52:52 +0900

    feat(sutera-lib): SuteraIdentity, SuteraSignedMessage (#2)

---
+++ sutera-lib/src/signature/identity.rs
@@ -0,0 +1,223 @@
+use std::str::FromStr;
+
+use ring_compat::signature::ed25519;
+use thiserror::Error;
+
+/// An error that occurs when parsing a Sutera identity string.
+#[derive(Debug, Error, PartialEq, Eq)]
+pub enum SuteraIdentityStringParseError {
+    #[error("invalid identity string")]
+    InvalidFormat,
+    #[error("invalid identity string, version {0} is not supported")]
+    VersionMismatch(String),
+    #[error("invalid identity string, kind {0} is not supported")]
+    UnsupportedKind(String),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, strum::EnumString, strum::AsRefStr)]
+pub enum SuteraIdentityKind {
+    #[strum(serialize = "user")]
+    User,
+}
+
+/// A struct representing an identity in the Sutera network.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SuteraIdentity {
+    /// The kind of object treated in the Sutera network (e.g., user, server, world etc.)
+    pub kind: SuteraIdentityKind,
+
+    /// The display name of the identity.  
+    /// This is only designated for human-readable purposes and plays no role in authentication.  
+    /// display_name can only contain alphanumeric characters (0-9, a-z)
+    pub display_name: Option<String>,
+
+    /// The ed25519 public key of the identity.  
+    /// This is used to verify the signature of the identity.
+    pub pub_signature: ed25519::VerifyingKey,
+}
+
+/// Convert SuteraIdentity to String.  
+/// The format is `{type}@{display_name}.sutera-identity-v1.{pub_signature}`.  
+/// Because pub_signature is 32byte, so the part `{pub_signature}` is 64 letters hexadecimal string.  
+///
+/// ## Example
+/// ```no_test
+/// user.sutera-identity-v1.fffffff.....
+/// user@alice.sutera-identity-v1.fffffff.....
+/// ```
+impl From<SuteraIdentity> for String {
+    fn from(identity: SuteraIdentity) -> String {
+        format!(
+            "{}.sutera-identity-v1.{}",
+            match identity.display_name {
+                Some(display_name) => format!("{}@{}", identity.kind.as_ref(), display_name),
+                None => identity.kind.as_ref().to_string(),
+            },
+            identity
+                .pub_signature
+                .0
+                .iter()
+                .fold(String::with_capacity(64), |acc, byte| acc
+                    + &format!("{:02x}", byte))
+        )
+    }
+}
+
+impl TryFrom<String> for SuteraIdentity {
+    type Error = SuteraIdentityStringParseError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let parts: Vec<&str> = value.split('.').collect();
+        if parts.len() != 3 {
+            return Err(SuteraIdentityStringParseError::InvalidFormat);
+        }
+
+        let (kind, version, pub_key) = (parts[0].to_string(), parts[1].to_string(), parts[2]);
+
+        if kind.is_empty() {
+            return Err(SuteraIdentityStringParseError::InvalidFormat);
+        }
+
+        if version != "sutera-identity-v1" {
+            return Err(SuteraIdentityStringParseError::VersionMismatch(version));
+        }
+
+        if pub_key.len() != 64 {
+            return Err(SuteraIdentityStringParseError::InvalidFormat);
+        }
+
+        let (kind, display_name) = match kind.find('@') {
+            Some(index) => (
+                SuteraIdentityKind::from_str(&kind[..index]).map_err(|_| {
+                    SuteraIdentityStringParseError::UnsupportedKind(kind[..index].to_string())
+                })?,
+                Some(kind[index + 1..].to_string()),
+            ),
+            None => (
+                SuteraIdentityKind::from_str(&kind).map_err(|_| {
+                    SuteraIdentityStringParseError::UnsupportedKind(kind.to_string())
+                })?,
+                None,
+            ),
+        };
+
+        let pub_key_bytes = parts[2]
+            .as_bytes()
+            .chunks(2)
+            .map(|chunk| u8::from_str_radix(std::str::from_utf8(chunk).unwrap(), 16))
+            .collect::<Result<Vec<u8>, std::num::ParseIntError>>()
+            .or(Err(SuteraIdentityStringParseError::InvalidFormat))?;
+
+        Ok(SuteraIdentity {
+            kind,
+            display_name,
+            pub_signature: ed25519::VerifyingKey(pub_key_bytes.try_into().unwrap()),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use ring_compat::signature::ed25519;
+
+    #[test]
+    fn sutera_identity_string() {
+        // ダミーの公開鍵(全てのビットが0)のSuteraIdentityを作成, 文字列に変換
+        let identity = SuteraIdentity {
+            kind: SuteraIdentityKind::User,
+            display_name: Some("see2et".to_string()),
+            pub_signature: ed25519::VerifyingKey([0; 32]),
+        };
+
+        let identity_str: String = identity.clone().into();
+        assert_eq!(
+            identity_str,
+            "user@see2et.sutera-identity-v1.0000000000000000000000000000000000000000000000000000000000000000"
+        );
+
+        // 変換した文字列をSuteraIdentityに戻し, オリジナルのSuteraIdentityと一致するか検証
+        let parsed_identity: SuteraIdentity = identity_str.try_into().unwrap();
+        assert_eq!(identity, parsed_identity);
+    }
+
+    #[test]
+    fn sutera_identity_string_without_name() {
+        // ダミーの公開鍵(全てのビットが0)のSuteraIdentityを作成, 文字列に変換
+        let identity = SuteraIdentity {
+            kind: SuteraIdentityKind::User,
+            display_name: None,
+            pub_signature: ed25519::VerifyingKey([0; 32]),
+        };
+
+        let identity_str: String = identity.clone().into();
+        assert_eq!(
+            identity_str,
+            "user.sutera-identity-v1.0000000000000000000000000000000000000000000000000000000000000000"
+        );
+
+        // 変換した文字列をSuteraIdentityに戻し, オリジナルのSuteraIdentityと一致するか検証
+        let parsed_identity: SuteraIdentity = identity_str.try_into().unwrap();
+        assert_eq!(identity, parsed_identity);
+    }
+
+    #[test]
+    fn sutera_identity_string_version_mismatch() {
+        // バージョンが異なる文字列をSuteraIdentityに変換しようとした場合のエラーを検証
+        let invalid_identity_str = "see2et.sutera-identity-v2.xxx";
+        assert_eq!(
+            SuteraIdentity::try_from(invalid_identity_str.to_string()),
+            Err(SuteraIdentityStringParseError::VersionMismatch(
+                "sutera-identity-v2".to_string()
+            ))
+        );
+    }
+
+    #[test]
+    fn sutera_identity_string_invalid() {
+        // 不正な文字列をSuteraIdentityに変換しようとした場合にエラーにちゃんとなるか検証
+
+        let invalid_identity_str = "user.sutera-identity-v1";
+        assert_eq!(
+            SuteraIdentity::try_from(invalid_identity_str.to_string()),
+            Err(SuteraIdentityStringParseError::InvalidFormat)
+        );
+        let invalid_identity_str = "user.sutera-identity-v1.abc";
+        assert_eq!(
+            SuteraIdentity::try_from(invalid_identity_str.to_string()),
+            Err(SuteraIdentityStringParseError::InvalidFormat)
+        );
+        let invalid_identity_str = "user.sutera-identity-v1.x000000000000000000000000000000000000000000000000000000000000000";
+        assert_eq!(
+            SuteraIdentity::try_from(invalid_identity_str.to_string()),
+            Err(SuteraIdentityStringParseError::InvalidFormat)
+        );
+        let invalid_identity_str = "unknown.sutera-identity-v1.x000000000000000000000000000000000000000000000000000000000000000";
+        assert_eq!(
+            SuteraIdentity::try_from(invalid_identity_str.to_string()),
+            Err(SuteraIdentityStringParseError::UnsupportedKind(
+                "unknown".to_string()
+            ))
+        );
+        let invalid_identity_str = "unknown@hello.sutera-identity-v1.x000000000000000000000000000000000000000000000000000000000000000";
+        assert_eq!(
+            SuteraIdentity::try_from(invalid_identity_str.to_string()),
+            Err(SuteraIdentityStringParseError::UnsupportedKind(
+                "unknown".to_string()
+            ))
+        );
+        let invalid_identity_str =
+            "sutera-identity-v1.0000000000000000000000000000000000000000000000000000000000000000";
+        assert_eq!(
+            SuteraIdentity::try_from(invalid_identity_str.to_string()),
+            Err(SuteraIdentityStringParseError::InvalidFormat)
+        );
+        let invalid_identity_str =
+            ".sutera-identity-v1.0000000000000000000000000000000000000000000000000000000000000000";
+        assert_eq!(
+            SuteraIdentity::try_from(invalid_identity_str.to_string()),
+            Err(SuteraIdentityStringParseError::InvalidFormat)
+        );
+    }
+}
---
+++ sutera-lib/src/signature/message.rs
@@ -0,0 +1,159 @@
+use std::str::FromStr;
+
+use super::identity::SuteraIdentity;
+use ring_compat::signature::{ed25519, Signer, Verifier};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+/// A common structure of message exchanged within the Sutera network.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SuteraSignedMessage {
+    pub author: SuteraIdentity,
+    pub message: String,
+    pub signature: ed25519::Signature,
+}
+
+/// An error that occurs when signing a message.
+#[derive(Debug, Error)]
+pub enum SuteraMessageSigningError {
+    #[error("the signing key does not match the author's verifying key")]
+    SigningKeyMismatch,
+}
+
+impl SuteraSignedMessage {
+    /// Sign a message with the author's signing key.
+    ///
+    /// ## Parameters
+    /// - `author`: The author of the message.
+    /// - `message`: The content of the message.
+    /// - `signer`: The signing key of the author.
+    ///
+    /// ## Returns
+    /// A new signed message.
+    /// if the signing key does not match the author's verifying key, return `Err`.
+    pub fn new(
+        author: SuteraIdentity,
+        message: String,
+        signer: ed25519::SigningKey,
+    ) -> Result<Self, SuteraMessageSigningError> {
+        let verifying_key = signer.verifying_key();
+        if verifying_key != author.pub_signature {
+            return Err(SuteraMessageSigningError::SigningKeyMismatch);
+        }
+
+        let signature = signer.sign(message.as_bytes());
+
+        Ok(SuteraSignedMessage {
+            author,
+            message,
+            signature,
+        })
+    }
+
+    /// Check if the signature is valid.
+    ///
+    /// **SuteraSignedMessage should be verified before processing the message.**
+    ///
+    /// **The signature is once checked at the time of creation in normal scenario,**
+    /// **so this method is used as a assertion.**
+    ///
+    /// ## Return
+    /// `true` if the signature is valid, otherwise `false`.
+    pub fn verify(&self) -> bool {
+        self.author
+            .pub_signature
+            .verify(self.message.as_bytes(), &self.signature)
+            .is_ok()
+    }
+}
+
+impl Serialize for SuteraSignedMessage {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let payload = SuteraSignedMessagePayload {
+            author: self.author.clone().into(),
+            message: self.message.clone(),
+            signature: self.signature.to_string(),
+        };
+        payload.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for SuteraSignedMessage {
+    fn deserialize<D>(deserializer: D) -> Result<SuteraSignedMessage, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let payload = SuteraSignedMessagePayload::deserialize(deserializer)?;
+        let author = SuteraIdentity::try_from(payload.author).map_err(serde::de::Error::custom)?;
+        let signature =
+            ed25519::Signature::from_str(&payload.signature).map_err(serde::de::Error::custom)?;
+
+        Ok(SuteraSignedMessage {
+            author,
+            message: payload.message,
+            signature,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct SuteraSignedMessagePayload {
+    pub author: String,
+    pub message: String,
+    pub signature: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::signature::identity::SuteraIdentityKind;
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use rand_core::{OsRng, RngCore};
+
+    fn test_signed_message() -> SuteraSignedMessage {
+        // ランダムな秘密鍵を生成
+        let mut ed25519_seed = [0u8; 32];
+        OsRng.fill_bytes(&mut ed25519_seed);
+        let secret = ed25519::SigningKey::from_bytes(&ed25519_seed);
+
+        // 秘密鍵からSuteraIdentityを生成
+        let identity = SuteraIdentity {
+            kind: SuteraIdentityKind::User,
+            display_name: Some("see2et".to_string()),
+            pub_signature: secret.verifying_key(),
+        };
+
+        // 適当なStringをメッセージとして用意し,署名する
+        let message = "Hello, Sutera!";
+        SuteraSignedMessage::new(identity, message.to_string(), secret).unwrap()
+    }
+
+    #[test]
+    fn sign_message() {
+        // ランダムな秘密鍵で署名されたメッセージを生成
+        let mut signed_message = test_signed_message();
+
+        // 署名検証を通過することを確認
+        assert!(signed_message.verify());
+
+        // 内容を変更し,改竄されたメッセージとして署名検証に失敗することを確認
+        signed_message.message = "Hello, Sutera?".to_string();
+        assert!(!signed_message.verify());
+    }
+
+    #[test]
+    fn signed_message_serializable() {
+        // ランダムな秘密鍵で署名されたメッセージを生成
+        let signed_message = test_signed_message();
+
+        // シリアライズ -> デシリアライズしても内容が変わらないことを確認
+        let serialized = serde_json::to_string(&signed_message).unwrap();
+        let deserialized: SuteraSignedMessage = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(signed_message, deserialized);
+    }
+}
