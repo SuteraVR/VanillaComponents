8148244e04180c1bb728869e281f10bccdef07b1
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-07-14 19:21:35 +0900

    chore: Init rust-overlay (#5)

--- flake.nix
+++ flake.nix
@@ -4,25 +4,50 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
         };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rust-beta = pkgs.rust-bin.beta.latest.default;
+        rust-nightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
       in {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            libiconv
-          ];
-          buildInputs = with pkgs; [
-            rustup
-          ];
-          shellHook = ''
-            exec $SHELL
-          '';
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.libiconv
+              rust
+            ];
+            shellHook = ''
+              exec $SHELL
+            '';
+          };
+
+          beta = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.libiconv
+              rust-beta
+            ];
+            shellHook = ''
+              exec $SHELL
+            '';
+          };
+
+          nightly = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.libiconv
+              rust-nightly
+            ];
+            shellHook = ''
+              exec $SHELL
+            '';
+          };
         };
       }
     );
