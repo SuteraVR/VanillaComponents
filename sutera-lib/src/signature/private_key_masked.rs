7390da5dc3cb6e457bea55c7e03bca98bf45f980
AsPulse / あすぱる <contact@aspulse.dev>
2025-01-14 17:40:30 +0900

    Add Tests and get, get_mut for PrivateKeyMasked

--- sutera-lib/src/signature/private_key_masked.rs
+++ sutera-lib/src/signature/private_key_masked.rs
@@ -5,6 +5,26 @@ use std::fmt::{self, Display};
 
 struct PrivateKeyMasked<T>(T);
 
+impl<T> PrivateKeyMasked<T> {
+    /// Get the inner value.
+    ///
+    /// Do not use this for error messages.
+    /// **Do not use this in argument or return type to any function.**
+    /// (tracing-subscriber might expose the inner value)
+    pub fn get_raw(&self) -> &T {
+        &self.0
+    }
+
+    /// Get the mutable reference to the inner value.
+    ///
+    /// Do not use this for error messages.
+    /// **Do not use this in argument or return type to any function.**
+    /// (tracing-subscriber might expose the inner value)
+    pub fn get_raw_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+}
+
 impl<T> Debug for PrivateKeyMasked<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
@@ -60,5 +80,47 @@ impl<T: PartialOrd> PartialOrd for PrivateKeyMasked<T> {
 impl<T: Ord> Ord for PrivateKeyMasked<T> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.0.cmp(&other.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn debug_print_should_masked_string() {
+        let debug_output = format!("{:?}", PrivateKeyMasked("SUPERSECRET".to_string()));
+        assert!(!debug_output.contains("SUPERSECRET"));
+        assert!(debug_output.contains("String"));
+        assert!(debug_output.contains("Masked, containing private key information."));
+    }
+
+    #[test]
+    fn debug_print_should_masked_u8slice() {
+        let hex: [u8; 4] = [0x01, 0x02, 0x03, 0x04];
+        let key: &PrivateKeyMasked<&[u8]> = &PrivateKeyMasked(&hex);
+        assert_eq!(
+            format!("{:?}", key),
+            "(Masked, containing private key information. <&[u8]>)"
+        );
+    }
+
+    #[test]
+    fn display_print_should_masked_string() {
+        let display_output = format!("{}", PrivateKeyMasked("SUPERSECRET".to_string()));
+        assert!(!display_output.contains("SUPERSECRET"));
+        assert!(display_output.contains("String"));
+        assert!(display_output.contains("Masked, containing private key information."));
+    }
+
+    #[test]
+    fn display_print_should_masked_u8slice() {
+        let hex: [u8; 4] = [0x01, 0x02, 0x03, 0x04];
+        let key: &PrivateKeyMasked<&[u8]> = &PrivateKeyMasked(&hex);
+        assert_eq!(
+            format!("{}", key),
+            "(Masked, containing private key information. <&[u8]>)"
+        );
     }
 }
